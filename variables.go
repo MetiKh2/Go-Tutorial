c05bbe36d9b5a0448046d7da6e476e380e66f8ce
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2023-01-14 23:42:20 +0330

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,36 @@
+package main
+import ("fmt")
+
+var a int
+var b int=2
+var c=3
+d:=1
+func main(){
+	a=1
+	 //var variablename type = value
+	 //variablename := value
+
+	 var student1 string="john" //type string
+	 var student2 ="jane" //type inferred
+	 x:=2 //type inferred
+
+	 fmt.Println(student1)
+	 fmt.Println(student2)
+	 fmt.Println(x)
+
+	//  var a string
+	//  var b int
+	//  var c bool
+
+	 fmt.Println(a)//""
+	 fmt.Println(b)
+	 fmt.Println(c)
+	 fmt.Println(d)
+	 
+
+	 var student3 string
+	 student3="Sarah"
+	 fmt.Println(student3)
+	 //:=
+
+}
