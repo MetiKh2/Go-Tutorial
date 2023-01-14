529ffdfc87476abfa5c116533a2c133f48443611
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2023-01-14 23:49:24 +0330

    Create variables2.go

---
+++ variables2.go
@@ -0,0 +1,21 @@
+package main
+import ("fmt")
+ 
+func main(){
+//  var a,b,c,d int =1,3,5,7
+//  var a,b =6,"Hello"
+//  c,d:=7,"World!" 
+ var (
+	a int
+	b int =1
+	c string ="hello"
+ )
+
+ fmt.Println(a)
+ fmt.Println(b)
+ fmt.Println(c)
+//age Age AGE
+//myVariableName = john
+//MyVariableName = john
+//my_variable_name=john
+}
