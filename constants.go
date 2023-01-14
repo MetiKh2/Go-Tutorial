e7f3bc4ffdc22e6ae578b3ae372408e703937d3c
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2023-01-14 23:54:56 +0330

    Create constants.go

---
+++ constants.go
@@ -0,0 +1,19 @@
+package main
+import ("fmt")
+ 
+const PI=3.14
+
+func main(){
+ //const CONSTNAME type = value
+ fmt.Println(PI)
+
+ const A int =1
+ fmt.Println(A)
+ 
+ const B =2
+ fmt.Println(B)
+
+ const C =3
+ C=4
+ fmt.Println(C)
+}
