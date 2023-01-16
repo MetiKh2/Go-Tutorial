5cb6fcedda6cf9c0bf9563903dd7cd9a77a46de3
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2023-01-16 17:09:44 +0330

    Create output.go

---
+++ output.go
@@ -0,0 +1,15 @@
+package main
+import ("fmt")
+ 
+
+func main(){
+  var i,j string ="Hello","World!"
+
+//   fmt.Print(i,"\n",j)
+//   fmt.Print(i," ",j)
+	//  fmt.Print(i,j)
+	// fmt.Println(i,j)
+	fmt.Printf("i has value %v and type %T \n",i,i)
+	fmt.Printf("j has value %v and type %T \n",j,j)
+
+}
