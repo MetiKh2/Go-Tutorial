793631ecd484c8c5ad7532492c7c5560967a6a70
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2023-01-16 22:23:05 +0330

    Create map.go

---
+++ map.go
@@ -0,0 +1,74 @@
+package main
+import ("fmt")
+ 
+ 
+
+func main(){
+ //var a =map[KeyType]ValueType{key1:value1,key2:value2,...}
+ //a :=map[KeyType]ValueType{key1:value1,key2:value2,...}
+//  var a =map[string]string{"brand":"Ford","model":"Mustang","year":"1964"}
+
+//  b:=map[string]int{"Oslo":1,
+// "Bergen":2,"Trondheim": 3, "Stavanger": 4}
+
+// fmt.Println(a)
+// fmt.Println(b)
+
+//var a =make(map[KeyType]ValueType)
+//b :=make(map[KeyType]ValueType)
+
+// var a =make(map[string]string)//the map is empty now
+// a["brand"] ="Ford"
+// a["model"] ="Mustang"
+// a["year"] ="1964"
+
+// b:=make(map[string]int)
+// b["Oslo"]=1
+// b["Bergen"]=2
+// b["Trondheim"]= 3
+//  b["Stavanger"]= 4
+
+//  fmt.Println(a)
+// fmt.Println(b)
+
+//var a map[keytype]valuetype
+// var a map[string]string
+// fmt.Println(a)
+	//value = map_name[key]
+// var a =make(map[string]string)//the map is empty now
+// a["brand"] ="Ford"
+// a["model"] ="Mustang"
+// a["year"] ="1964"
+// //map_name[key]=value
+// a["year"]="1970"//updating an element
+// a["color"]="red"//adding an element
+// delete(a,"year")
+ 
+//val,ok:=map_name[key]
+// val1,ok1:=a["brand"]
+// val2,ok2:=a["color"]
+// val3,ok3:=a["day"]
+// val4,ok4:=a["model"]
+// _,ok5:=a["year"]
+ 
+// fmt.Println(val1,ok1)
+// fmt.Println(val2,ok2)
+// fmt.Println(val3,ok3)
+// fmt.Println(val4,ok4)
+// fmt.Println(ok5)
+
+// b:=a
+// fmt.Println(a)
+// fmt.Println(b)
+
+// b["color"]="blue"
+// fmt.Println("after change b")
+// fmt.Println(a)
+// fmt.Println(b)
+
+// for k,v := range a{
+// 	fmt.Println(k,v)
+// }
+ 
+ 
+}
